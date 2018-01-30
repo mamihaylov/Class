b01c00538c879d853790d8e9b82853ce9819db6f
mamihaylov <maximmihkailov@gmail.com>
2018-01-30 19:11:53 +0700

    Class c#

---
+++ SimpleClassExample/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Car
+    { // 'Состояние ' обьекта car.
+        public string petName;
+        public int currSpeed;
+        // Специальный стандартный конструктор.
+
+        // Функциональность car
+        public void PrintState()
+        {
+            Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed);
+        }
+        public void SpeedUp(int delta)
+        {
+            currSpeed += delta;
+        }
+       
+    }
+    
+    
+}
---
+++ SimpleClassExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Fun with Class types****\n");
+            // разместить в памяти и сконфигурировать обьект Car.
+            Car myCar = new SimpleClassExample.Car();
+            myCar = new Car();
+            myCar.petName = "henry";
+            myCar.currSpeed = 10;
+            // Увеличить скорость автомобиля в несколько раз и вывести новое состояние.
+            for (int i=0; i<= 10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.PrintState();
+            }
+            Console.ReadLine();
+        }
+    }
+}

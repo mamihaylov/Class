6aef6490aa17b82837bd336aafcf8541e3390fda
mamihaylov <maximmihkailov@gmail.com>
2018-02-01 16:39:24 +0700

    Add new project and add Class "Saving Account"

---
+++ StaticDataAndMembers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun whth Static Data ****\n");
+            SavingAccount s1 = new SavingAccount(50);
+            SavingAccount s2 = new SavingAccount(100);
+            SavingAccount s3 = new SavingAccount(10000.75);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticDataAndMembers/SavingAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    // Класс депозитного счета.
+    class SavingAccount
+    {   
+        // Данные уровня экземпляра.
+        public double currBalance;
+        //Статический элемент данных.
+        public static double currInterestRate = 0.04;
+        public SavingAccount(double balance)
+        {
+            currBalance = balance;
+        }
+        
+    }
+}

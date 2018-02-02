c5429d36a37238249e76349d65f72b568e985748
mamihaylov <maximmihkailov@gmail.com>
2018-02-02 17:30:26 +0700

    New project< add class Point.

---
+++ ObjectInitializers/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializers
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point (int xVal, int YVal)
+        {
+            X = xVal;
+            Y = YVal; 
+        }
+        // Конструктор
+        public Point () { }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("[{0},{1}]", X, Y);
+        }
+    }
+}
---
+++ ObjectInitializers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun with Object Init Syntax****\n");
+            // Создать обьект Point с установкой кажого свойства вручную.
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+            // Создаем обьект с исп.спец конструктора.
+            Point anotherPoint = new Point(20, 20);
+            anotherPoint.DisplayStats();
+
+            // Создать обьект с использованием синтаксиса инициализатора обьекта.
+            Point finalPoint = new Point { X = 30, Y = 30 };
+            finalPoint.DisplayStats();
+            Console.ReadLine();
+
+        }
+    }
+}

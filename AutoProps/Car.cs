516de6c26113f129df96ceaa355077e3e69394f7
mamihaylov <maximmihkailov@gmail.com>
2018-02-02 17:14:05 +0700

    New project AutoProps. Create a "Car" class with automatic properties.

---
+++ AutoProps/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProps
+{
+    class Car
+    {
+        public string PetName { get; set; }
+        public int Speed { get; set; }
+        public string Color { get; set; }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("CarName : {0}",PetName );
+            Console.WriteLine("Speed: {0}", Speed);
+            Console.WriteLine("Color : {0}",Color);
+        }
+    }
+}
---
+++ AutoProps/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun with Automatic properties****\n");
+            Car c = new Car();
+            c.PetName = "Frank";
+            c.Speed = 55;
+            c.Color = "Red";
+            Console.WriteLine(" Your car is named {0}? Thath's odd...",c.PetName );
+            c.DisplayStats();
+            Console.ReadLine();
+        }
+    }
+}

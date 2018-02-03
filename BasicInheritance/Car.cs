7dbd5e061645131a6d5b3bfdecb4626566441051
mamihaylov <maximmihkailov@gmail.com>
2018-02-03 10:00:26 +0700

    New project. Add " Car" class(lab3).

---
+++ BasicInheritance/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    // Простой базовый класс.
+    class Car
+    {
+        public readonly int maxSpeed; // поле для только чтения.
+        private int currSpeed; // закрытое поле.
+
+        //конструкторы
+        public Car(int max)
+        {
+            maxSpeed = max;
+
+        }
+
+        public Car()
+        {
+            maxSpeed = 55;
+        }
+        public int Speed // Открытое свойство.
+        {
+            get { return currSpeed; }
+            set
+            {
+                currSpeed = value;
+
+                if (currSpeed > maxSpeed )
+                {
+                    currSpeed = maxSpeed;
+                }
+
+            }
+        }
+    }
+}
---
+++ BasicInheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Basic Inheritance ****\n");
+            // Создаем экземпляр типа Car и устанавливаем max. Скорость
+            Car myCar = new Car(80);
+            //Установить текущую скорость и вывести е ена консоль.
+            myCar.Speed = 50;
+            Console.WriteLine("My car is going {0} MPH", myCar.Speed);
+            Console.ReadLine();
+
+        }
+    }
+}

89444675a2d34b476ba5c54c84bdd06c88058c29
mamihaylov <maximmihkailov@gmail.com>
2018-02-07 10:32:06 +0700

    New project "Shapes" add "Shape,Circle, Hexagon" class

---
+++ Shapes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Fun with Polymorphism ***\n");
+            // Создать массив совместимых с Shape объектов.
+            Shape[] myShape = { new Hexagon(), new Circle(),new Hexagon("Mick"),
+                                new Circle("Beth"), new Hexagon("Linda")};
+            // пройти в цикле по всем эелементам и взаимодействовать 
+            // С полиморфным интерфесом.
+            foreach(Shape s in myShape)
+            {
+                s.Draw();
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

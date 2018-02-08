0285e4a533913c4a47a66abd40289782f44fea8e
mamihaylov <maximmihkailov@gmail.com>
2018-02-08 17:19:12 +0700

    UpDate "Car" Class. add exception handler.

--- SimpleException/Program.cs
+++ SimpleException/Program.cs
@@ -7,15 +7,31 @@ using System.Threading.Tasks;
 namespace SimpleException
 {
     class Program
-    {
+    {   // Обработка сгенерированного исключения.
         static void Main(string[] args)
         {
             Console.WriteLine("**** simple Exception Example ****\n");
             Console.WriteLine("=> Creating a car and stepping on it");
             Car myCar = new Car("Zippy", 20);
             myCar.CrankTunes(true);
-            for (int i = 0; i < 10; i++)
-                myCar.Accelerate(10);
+
+            /* Разогнать до скорости, превышающей максимальны
+               предел автомобиля выдачи исключения. */
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                    myCar.Accelerate(10);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\n *** Error! ***"); // Ошибка
+                Console.WriteLine("Method : {0}", e.TargetSite); // Метод
+                Console.WriteLine("Message : {0}", e.Message); // сообщение
+                Console.WriteLine("Source : {0}", e.Source); // источник
+            }
+            /* Ошибка была обработанна, продолжается выполнение
+             * следующего оператора.*/
+            Console.WriteLine("\n**** out of exception logic ****");
             Console.ReadLine();
 
         }

a0802b44a02a745552d127e862c6b5bd13f624fd
mamihaylov <maximmihkailov@gmail.com>
2018-02-08 16:48:05 +0700

    New project "SimpleException", add "Car" and "Radio" class.

---
+++ SimpleException/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Car
+    {
+        // Константа для предстовление максимальной скорости
+        public const int MaxSpeed = 100;
+
+        //Свойства автомобиля.
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+
+        //не вышел ли автомобил из строя?
+        private bool carIsDead;
+
+        // Автомобиль имеет радиоприемник
+        private Radio theMusicBox = new Radio();
+
+        // Конструкторы
+        public Car () { } //1. констр.
+        public Car (string name, int speed) // 2. констр.
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+        public void CrankTunes(bool state)
+        {
+            // делегировать запрос внутреннему объекту.
+            theMusicBox.TurnOn(state);
+
+        }
+        // Проверить не перегрелся ли автомобиль.
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order. . .", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    Console.WriteLine("{0} has overheated!", PetName);
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);            }
+        }
+        
+
+    }
+}
---
+++ SimpleException/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** simple Exception Example ****\n");
+            Console.WriteLine("=> Creating a car and stepping on it");
+            Car myCar = new Car("Zippy", 20);
+            myCar.CrankTunes(true);
+            for (int i = 0; i < 10; i++)
+                myCar.Accelerate(10);
+            Console.ReadLine();
+
+        }
+    }
+}

c8a2b9b2644698c1ba54c774ac96edb883c05863
mamihaylov <maximmihkailov@gmail.com>
2018-02-08 15:41:09 +0700

    New project ObjectOverrides. Add "Person" class.

---
+++ ObjectOverrides/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Person
+    {
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+            public Person(string fname,string lName,int personAge)
+        {
+            FirsName = fname;
+            LastName = lName;
+            Age = personAge;
+
+        }
+
+
+    }
+}
---
+++ ObjectOverrides/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun with system.object ****\n");
+            Person p1 = new Person();
+            // Использовать унаследованные члены System.Object.
+            Console.WriteLine("ToString: {0}", p1.ToString());
+            Console.WriteLine("Hash code: {0}", p1.GetHashCode());
+            Console.WriteLine("Type: {0}", p1.GetType());
+
+            // Создать другую ссылку на p1
+            Person p2 = p1;
+            object o = p2;
+            // Указывают ли ссылки на олин и тот же обьект в памяти?
+            if (o.Equals(p1) && p2.Equals(o))
+            {
+                Console.WriteLine("Same instance!"); // Один и тот же экземпляр
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
